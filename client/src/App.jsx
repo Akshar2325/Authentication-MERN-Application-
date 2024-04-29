6c80b2aaf4dce5589951967915de336f60bbb94a
Akshar2325 <aksharbhesaniya@gmail.com>
2024-04-29 16:48:23 +0530

    Create header componets

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,13 @@ import About from './Pages/About';
 import SignIn from './Pages/SignIn';
 import SignUp from './Pages/SignUp';
 import Profile from './Pages/Profile';
+import Header from './Components/Header';
 
 const App = () => {
   return <BrowserRouter>
+
+    {/* Header */}
+    <Header />
 
     <Routes>
       <Route path='/' element={<Home />} />
